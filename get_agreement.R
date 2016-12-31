617f4f02451ddf8f0a274b7232e47bf281ba3312
sdentro <sd11@sanger.ac.uk>
2016-12-31 20:13:53 +0000

    Added some debug

--- get_agreement.R
+++ get_agreement.R
@@ -218,9 +218,10 @@ get_frac_genome_agree_maj_vote = function(samplename, all_data, segments, min_me
     
     # Work out what is the majority vote and by how many samples - for only the clonal fits
     methods_with_clonal_solution = colnames(combined_status)[!is.na(combined_status[i,]) & combined_status[i,]=="clonal"]
-    
+    print(methods_with_clonal_solution)
+    print(length(methods_with_clonal_solution))
     # Check that we've got enough methods with a solution - we'll check for enough methods agreeing below
-    if (nrow(all_states) > 0 & length(methods_with_clonal_solution) >= min_methods_with_call_on_segment) {
+    if (length(methods_with_clonal_solution) >= min_methods_with_call_on_segment) {
       clonal_solutions = all_states[row.names(all_states) %in% methods_with_clonal_solution, ]
       
       # Choose, if a consensus is reached
