5f02273afbaec8412f1015126805c7d47389c6cb
sdentro <sd11@sanger.ac.uk>
2017-01-17 13:50:54 +0000

    Fixing issue where dkfz was fill in option and two subclonal states were expected

--- get_agreement.R
+++ get_agreement.R
@@ -326,6 +326,9 @@ get_frac_genome_agree_rounded = function(samplename, all_data_clonal, all_data_1
           
           if (method %in% clonal_votes) {
             hits_inventory[j, method] = as.numeric(cn_state_1)
+          } else if (method=="dkfz") {
+            # DKFZ does not report multiple subclonal states
+            hits_inventory[j, method] = as.numeric(cn_state_1)
           } else {
             hits_inventory[j, method] = as.numeric(cn_state_1 | cn_state_2)
           }
@@ -1138,7 +1141,7 @@ if (file.exists(breakpoints_file)) {
     closest_method_index = which(grepl(paste0("map_", closest_method), names(all_data_clonal))  & !grepl("baflogr", names(all_data_clonal)))
     closest_method_profile = all_data_clonal[[closest_method_index]]$cn_states
     for (i in 1:nrow(consensus_profile)) {
-
+      print(i)
       if (is.na(consensus_profile$major_cn[i]) && is.na(consensus_profile$minor_cn[i])) {
         
         # Check if the method is allowed to make a call singlehandidly on this segment
